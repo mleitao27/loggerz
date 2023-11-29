78854227b81abb8947d64bb2d34d7e4fadcbc3d4
Miguel Leitão <mleitao@Miguels-MBP.Home>
2023-11-29 15:46:21 +0000

    log list

--- loggerz-app/src/pages/Home.vue
+++ loggerz-app/src/pages/Home.vue
@@ -1,13 +1,42 @@
 <template>
-    <div class="bg-stone-400">
-        HOME
+    <div class="p-6">
+        <div class="grid grid-cols-4 gap-4 bg-yellow-400 p-2 rounded">
+            <p>Level</p>
+            <p>Message</p>
+            <p>Component</p>
+            <p>Created at</p>
+        </div>
+        <div v-for="(log, index) in logs" :key="log._id" :class="index % 2 != 0 ? 'bg-yellow-50' : 'bg-yellow-200'" class="p-2 rounded col-span-4 grid grid-cols-4 gap-4">
+            <p>{{log.level}}</p>
+            <p>{{log.message}}</p>
+            <p>{{log.component}}</p>
+            <p>{{log.created_at}}</p>
+        </div>
     </div>
 </template>
 
 <script>
-    export default {
-        
+import { onMounted, computed } from 'vue'
+import { useStore } from 'vuex'
+
+export default {
+    setup() {
+        const store = useStore()
+
+        onMounted(async () => {
+            console.log('here1')
+            await store.dispatch('fetchLogs')
+        })
+
+        const logs = computed(() => {
+            return store.state.logs.logs
+        })
+
+        return {
+            logs
+        }
     }
+}
 </script>
 
 <style lang="scss" scoped>
--- loggerz-app/src/store/modules/logs.js
+++ loggerz-app/src/store/modules/logs.js
@@ -1,6 +1,25 @@
+import axios from 'axios'
 export default {
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    logs: []
+  },
+  mutations: {
+    SET_LOGS(state, logs) {
+      state.logs = logs
+    },
+  },
+  actions: {
+    fetchLogs({ commit }) {
+      console.log('here2')
+      return axios
+        .get('http://localhost:5001/api/v1/logs/')
+        .then(({data}) => {
+          commit('SET_LOGS', data)
+        })
+        .catch((e) => {
+          console.log(e)
+        })
+    },
+  },
   getters: {}
 }
